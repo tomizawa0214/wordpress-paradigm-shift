e7a34d6b9803389bccbccdf2d6909f4b31876c86
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-10-10 18:04:02 +0900

    トップページをテンプレ化

---
+++ paradigm_shift/front-page.php
@@ -0,0 +1,53 @@
+<?php get_header(); ?>
+  <!-- コンテンツエリア -->
+  <section id="contents">
+
+  <!--メイン画像-->
+  <img id="main_visual" class="inner" src="http://paradigmshift.local/wp-content/uploads/2020/10/main_visual.jpg" alt="株式会社ParadigmShift">
+
+  <!--お知らせ-->
+  <h2>Information</h2>
+  <div class="inner">
+    <ul id="infolist">
+      <li>
+        <div class="thumb"><img src="http://paradigmshift.local/wp-content/uploads/2020/10/webshop-min.jpg" alt=""></div>
+        <div class="date">2020/09/10</div>
+        <div class="title"><a href="http://paradigmshift.local/estore-ranking-3">国内大手Eストアにて売上3位を獲得しました。</a></div>
+      </li>
+      <li>
+        <div class="thumb"><img src="http://paradigmshift.local/wp-content/uploads/2020/10/conosulting-min.jpg" alt=""></div>
+        <div class="date">2020/06/03</div>
+        <div class="title"><a href="http://paradigmshift.local/consulting-over-ten-companies">コンサルティング契約社数が10社を超えました。</a></div>
+      </li>
+      <li>
+        <div class="thumb"><img src="http://paradigmshift.local/wp-content/uploads/2020/05/new_product.jpg" alt=""></div>
+        <div class="date">2020/05/02</div>
+        <div class="title"><a href="http://paradigmshift.local/original-brand">当社オリジナルブランド商品の企画をスタートしました。</a></div>
+      </li>
+      <li>
+        <div class="thumb"><img src="http://paradigmshift.local/wp-content/uploads/2020/05/renewal.jpg" alt=""></div>
+        <div class="date">2020/04/10</div>
+        <div class="title"><a href="http://paradigmshift.local/homepage-renewal">ホームページを公開しました。</a></div>
+      </li>
+    </ul>
+  </div>
+
+  <!--事業内容-->
+  <h2>株式会社ParadigmShiftの事業内容</h2>
+  <div id="our-business" class="inner">
+    <div class="biz-box col-4">
+      <h3>海外輸入小売事業</h3>
+      <a href="http://paradigmshift.local/business"><img class="photoframe" src="http://paradigmshift.local/wp-content/uploads/2020/10/import-min.jpg" alt="海外輸入小売事業"></a>
+    </div>
+    <div class="biz-box col-4">
+      <h3>インターネット通販事業</h3>
+      <a href="http://paradigmshift.local/business"><img class="photoframe" src="http://paradigmshift.local/wp-content/uploads/2020/10/webshop-min.jpg" alt="インターネット通販事業"></a>
+    </div>
+    <div class="biz-box col-4">
+      <h3>コンサルティング事業</h3>
+      <a href="http://paradigmshift.local/business"><img class="photoframe" src="http://paradigmshift.local/wp-content/uploads/2020/10/conosulting-min.jpg" alt="コンサルティング事業"></a>
+    </div>
+  </div>
+  </section>
+  <!-- //コンテンツエリア -->
+<?php get_footer(); ?>
