7bc507474771ae616d8028e68929adcf14625865
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-10-10 15:35:15 +0900

    404ページ

---
+++ paradigm_shift/404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+  <!-- コンテンツエリア -->
+  <section id="contents">
+    <!--ページのタイトル-->
+    <h2>お探しのページが見つかりませんでした。</h2>
+    <!--ページの本文-->
+    <a class="readmore" href="<?php echo home_url(); ?>">ホーム</a>
+  </section>
+  <!--//コンテンツエリア-->
+<?php get_footer(); ?>
