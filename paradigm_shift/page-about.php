29c0d7c28776a7cec243c13ba738ae2beff5fe67
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-10-12 18:29:33 +0900

    会社概要にカスタムフィールドを適用

---
+++ paradigm_shift/page-about.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+  <!-- コンテンツエリア -->
+  <section id="contents">
+    <!-- 記事のループ -->
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <!--記事-->
+    <article>
+      <!--ページのタイトル-->
+      <h2><?php the_title(); ?></h2>
+      <!--ページの本文-->
+      <div class="entry-content">
+        <table id="company-prof" class="inner">
+          <?php
+            $rows = CFS()->get('loop_profile');
+            foreach ($rows as $row) :
+          ?>
+          <tr>
+            <th><?php echo $row['txt_profile_name']; ?></th>
+            <td><?php echo $row['txt_profile_content']; ?></td>
+          </tr>
+          <?php endforeach; ?>
+        </table>
+      </div>
+    </article>
+    <!--//記事-->
+    <?php endwhile; endif; ?>
+    <!-- 記事のループ -->
+  </section>
+  <!--//コンテンツエリア-->
+<?php get_footer(); ?>
